11670c75f92f3e174d31abb8573e8374789d32ae
solomiia_borukh <s.borukh@rubylabs.com>
2026-06-30 17:36:55 +0300

    fix toggle theme

--- src/pkg/theme/theme.toggle.tsx
+++ src/pkg/theme/theme.toggle.tsx
@@ -2,32 +2,21 @@
 
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
 
 import { Button } from './ui/button'
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
 
   return (
     <Button
       variant="outline"
+      size="icon"
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {mounted ? (
-        theme === 'dark' ? (
-          <Sun className="size-5" />
-        ) : (
-          <Moon className="size-5" />
-        )
-      ) : (
-        <span className="size-5" />
-      )}
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
     </Button>
   )
 }
